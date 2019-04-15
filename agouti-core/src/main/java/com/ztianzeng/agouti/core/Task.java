adad33e1b7120e37042ceca60ad7e51a78048f0c
zxcvbnmzsedr <tianzneg@gmail.com>
2019-04-15 14:25:33 +0800

    License

--- agouti-core/src/main/java/com/ztianzeng/agouti/core/Task.java
+++ agouti-core/src/main/java/com/ztianzeng/agouti/core/Task.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2018-2019 zTianzeng Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.ztianzeng.agouti.core;
 
 import com.alibaba.fastjson.JSONObject;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -37,6 +54,8 @@ public class Task {
     private Map<String, Object> originInputs;
 
     private Map<String, Object> inputs;
+
+    private Map<String, Object> inputData = new HashMap<>();
 
 
     public enum TaskType {
---
+++ agouti-http/src/main/java/com/ztianzeng/agouti/http/HttpTask.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018-2019 zTianzeng Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.ztianzeng.agouti.http;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ztianzeng.agouti.core.Task;
+import com.ztianzeng.agouti.core.WorkFlow;
+import com.ztianzeng.agouti.core.WorkFlowTask;
+import com.ztianzeng.agouti.core.executor.http.HttpMethod;
+import lombok.Data;
+
+/**
+ * @author zhaotianzeng
+ * @version V1.0
+ * @date 2019-04-15 11:51
+ */
+public class HttpTask extends WorkFlowTask {
+    public static final String REQUEST_PARAMETER_NAME = "http_request";
+
+    protected ObjectMapper om = objectMapper();
+
+    @Override
+    public void start(WorkFlow workflow, Task task) {
+        // request info
+        Object request = task.getInputData().get(REQUEST_PARAMETER_NAME);
+
+        Input input = om.convertValue(request, Input.class);
+
+
+    }
+
+    @Data
+    public static class Input {
+
+        private HttpMethod method;
+
+        private String uri;
+
+        private Object body;
+    }
+
+
+    private static ObjectMapper objectMapper() {
+        final ObjectMapper om = new ObjectMapper();
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        om.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
+        om.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+        om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        om.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        return om;
+    }
+}
